05073df41e31c9b3fbbcd0593da15b802378cd53
DeepaliJha <92468192+DeepaliJha@users.noreply.github.com>
2022-10-01 21:15:30 +0530

    Create  Convert SortedListtoBinarySearchTree.cpp

---
+++ Convert SortedListtoBinarySearchTree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) 
+    {
+        if (!head) return NULL;
+        if (!head->next) return new TreeNode(head->val);
+        ListNode* slow=head,*fast=head->next;
+        while(fast->next && fast->next->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* mid=slow->next;
+        slow->next=NULL;
+        TreeNode* root=new TreeNode(mid->val);
+        root->left=sortedListToBST(head);
+        root->right=sortedListToBST(mid->next);
+        return root;
+    }
+};
